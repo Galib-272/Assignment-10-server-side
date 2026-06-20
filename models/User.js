6cc5ddd6d044abf9d976f60072d146f4ad44c25a
galib <ahmedgalib6677@gmail.com>
2026-06-20 19:17:53 +0600

    users.js updated

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["User", "Vendor", "Admin"], default: "User" }
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["User", "Vendor", "Admin"], default: "User" },
+  },
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("User", UserSchema);
